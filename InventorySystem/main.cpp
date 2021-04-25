d335890ff718b18e1d251fab65f706d32e1c25fc
K3nFischer <kendalljfischer@gmail.com>
2021-04-25 15:28:41 -0700

    test

--- InventorySystem/main.cpp
+++ InventorySystem/main.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
-
+#include <iostream>
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+    std::cout << "hello" << std::endl;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
